748899e43afd98606e65a6ca9cb87ca81f1d53dc
Ian Good <Ian.M.Good@gmail.com>
2024-09-02 23:04:24 -0700

    Update IGRangesInternal.h

--- Source/IGRanges/Private/IGRangesInternal.h
+++ Source/IGRanges/Private/IGRangesInternal.h
@@ -4,11 +4,5 @@
 
 #include "Logging/LogMacros.h"
 
-/*
-#ifndef WITH_DEV_AUTOMATION_TESTS
-#define WITH_DEV_AUTOMATION_TESTS 1
-#endif
-//*/
-
 DECLARE_LOG_CATEGORY_EXTERN(LogIGRanges, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogIGRangesTests, Log, All);
