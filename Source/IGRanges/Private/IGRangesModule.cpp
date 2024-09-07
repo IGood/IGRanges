46e7f05f1ddace059805f4847aa62f2058c63737
Ian Good <Ian.M.Good@gmail.com>
2024-09-07 15:28:21 -0700

    removed module impl
    no longer needed
    just had it for initial testing

--- Source/IGRanges/Private/IGRangesModule.cpp
+++ Source/IGRanges/Private/IGRangesModule.cpp
@@ -7,24 +7,4 @@
 DEFINE_LOG_CATEGORY(LogIGRanges);
 DEFINE_LOG_CATEGORY(LogIGRangesTests);
 
-namespace IG::Ranges
-{
-class FModule : public IModuleInterface
-{
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
-};
-
-void FModule::StartupModule()
-{
-	UE_LOG(LogIGRanges, Log, TEXT("Hello, World!"));
-}
-
-void FModule::ShutdownModule()
-{
-	UE_LOG(LogIGRanges, Log, TEXT("Goodbye, World!"));
-}
-
-} // namespace IG::Ranges
-
-IMPLEMENT_MODULE(IG::Ranges::FModule, IGRanges)
+IMPLEMENT_MODULE(FDefaultModuleImpl, IGRanges)
