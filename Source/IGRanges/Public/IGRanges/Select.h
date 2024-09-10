5cdc713c751f59d24b694b2e563a8a73126a3715
Ian Good <Ian.M.Good@gmail.com>
2024-09-10 01:16:21 -0700

    Update Select.h
    added `SelectNonNull` because that's a pretty common transform/filter combo

--- Source/IGRanges/Public/IGRanges/Select.h
+++ Source/IGRanges/Public/IGRanges/Select.h
@@ -2,7 +2,10 @@
 
 #pragma once
 
+#include "IGRanges/NonNull.h"
 #include <ranges>
+
+#include "IGRanges/Impl/Prologue.inl"
 
 namespace IG::Ranges
 {
@@ -12,4 +15,12 @@ template <class _Fn>
 	return std::views::transform(std::forward<_Fn>(_Fun));
 }
 
+template <class _Fn>
+[[nodiscard]] constexpr auto SelectNonNull(_Fn&& _Fun)
+{
+	return _IGR Select(std::forward<_Fn>(_Fun)) | _IGR NonNull();
+}
+
 } // namespace IG::Ranges
+
+#include "IGRanges/Impl/Epilogue.inl"
